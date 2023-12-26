a592037f0c0e5deebca6ba0f635fe19437f9ffa7
SashDu <sashadubina1996@gmail.com>
2023-12-26 06:24:42 +0300

    Did maine app and connected some templates, did some tests with templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request) -> HttpResponse:
+    context: dict[str, str] = {
+        'title': 'Home',
+        'content': 'Welcome to',
+        'list': ['first', 'second', 'third'],
+        'dict': {'first': 1},
+        'is_authenticated': False
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request) -> HttpResponse:
+    return HttpResponse("About page")
