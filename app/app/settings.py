b748d1d3074302432463457364071780c569deec
SashDu <sashadubina1996@gmail.com>
2024-04-01 01:41:01 +0300

    simple search created

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'home',
         'USER': 'home',
-        'PASSWORD': 'home',
+        'PASSWORD': 'hom',
         'HOST': 'localhost',
         'PORT': '5432',
     }
